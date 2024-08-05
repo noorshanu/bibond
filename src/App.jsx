b460ed0a9f82d538181c6b6286d9ca85d7f26842
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-08-06 02:11:30 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar"
+
+
+function App() {
+
+
+  return (
+    <>
+  <div className=" main">
+
+    <Navbar/>
+
+  </div>
+    </>
+  )
+}
+
+export default App
